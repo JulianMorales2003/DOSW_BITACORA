ea40f2d0f2b46e0ea53dfb6ea46681d4a58c4b2e
Julian Morales <julian.morales-z@mail.escuelaing.edu.co>
2026-08-31 12:44:43 -0500

    feat: ejercicio 8 semana 2 - pokemon

---
+++ src/main/dosw/semana_2/pokemon/Ejercicio8.java
@@ -0,0 +1,32 @@
+package dosw.semana_2.pokemon;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Ejercicio 08 - Evoluciones Preparadas
+ * Operador: filter()
+ */
+public class Ejercicio8 {
+
+    record PokemonEvolucion(String nombre, boolean puedeEvolucionar) {}
+
+    public static void main(String[] args) {
+        List<PokemonEvolucion> equipo = List.of(
+                new PokemonEvolucion("Pikachu", true),
+                new PokemonEvolucion("Raichu", false),
+                new PokemonEvolucion("Charmander", true),
+                new PokemonEvolucion("Charizard", false),
+                new PokemonEvolucion("Squirtle", true),
+                new PokemonEvolucion("Blastoise", false)
+        );
+
+        List<String> listos = equipo.stream()
+                .filter(PokemonEvolucion::puedeEvolucionar)
+                .map(PokemonEvolucion::nombre)
+                .collect(Collectors.toList());
+
+        System.out.println("Listos para evolucionar: " + listos);
+    }
+}
+
